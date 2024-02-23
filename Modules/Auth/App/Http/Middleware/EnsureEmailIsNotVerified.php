cc533dc871d1a0e7ce318f3753a9b7ee2ee36bb1
1970Mr <mrepoch1970@gmail.com>
2024-02-23 20:14:30 +0330

    Add ensure email not verified to Auth module

---
+++ Modules/Auth/App/Http/Middleware/EnsureEmailIsNotVerified.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Modules\App\Auth\Http\Middleware;
+use Closure;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\URL;
+
+class EnsureEmailIsNotVerified
+{
+    /**
+     * Specify the redirect route for the middleware.
+     *
+     * @param string $route
+     * @return string
+     */
+    public static function redirectTo($route)
+    {
+        return static::class . ':' . $route;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param string|null $redirectToRoute
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse|null
+     */
+    public function handle($request, Closure $next, $redirectToRoute = null)
+    {
+        if (!$request->user() ||
+            ($request->user() instanceof MustVerifyEmail &&
+                $request->user()->hasVerifiedEmail())) {
+            return $request->expectsJson()
+                ? abort(403, 'Your email address is verified.')
+                : Redirect::guest(URL::route($redirectToRoute ?: 'login'));
+        }
+
+        return $next($request);
+    }
+}
