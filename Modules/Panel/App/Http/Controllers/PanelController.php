5ef492b0f14cc865a33bd4b0060a2dfe6532ce71
1970Mr <mrepoch1970@gmail.com>
2024-02-26 16:08:31 +0330

    Add route and controller for Panel module

--- Modules/Panel/App/Http/Controllers/PanelController.php
+++ Modules/Panel/App/Http/Controllers/PanelController.php
@@ -3,65 +3,12 @@
 namespace Modules\Panel\App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Contracts\View\View;
 
 class PanelController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function __invoke(): View
     {
         return view('panel::index');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('panel::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request): RedirectResponse
-    {
-        //
-    }
-
-    /**
-     * Show the specified resource.
-     */
-    public function show($id)
-    {
-        return view('panel::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit($id)
-    {
-        return view('panel::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id): RedirectResponse
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- Modules/Panel/routes/web.php
+++ Modules/Panel/routes/web.php
@@ -3,17 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Panel\App\Http\Controllers\PanelController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::group([], function () {
-    Route::resource('panel', PanelController::class)->names('panel');
+    Route::get('panel', PanelController::class)->name('panel.index');
 });
