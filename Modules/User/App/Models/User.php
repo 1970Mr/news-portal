8fec9322a1d2be7606362ba18061882a63dcb369
1970Mr <mrepoch1970@gmail.com>
2024-02-26 15:59:51 +0330

    Add User module

--- Modules/User/App/Models/User.php
+++ Modules/User/App/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace Modules\User\App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
--- Modules/User/Database/Factories/UserFactory.php
+++ Modules/User/Database/Factories/UserFactory.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Database\Factories;
+namespace Modules\User\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Modules\User\App\Models\User>
  */
 class UserFactory extends Factory
 {
