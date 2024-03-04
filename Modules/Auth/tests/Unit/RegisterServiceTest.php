8a4a75dd789c60f91afb3424c0cbafe83114866e
1970Mr <mrepoch1970@gmail.com>
2024-03-04 21:18:42 +0330

    Add it_can_register_user test to RegisterServiceTest

--- Modules/Auth/tests/Unit/RegisterServiceTest.php
+++ Modules/Auth/tests/Unit/RegisterServiceTest.php
@@ -2,6 +2,8 @@
 
 namespace Modules\Auth\tests\Unit;
 
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Hash;
 use Modules\Auth\App\Http\Requests\RegisterRequest;
 use Modules\Auth\App\Services\RegisterService;
@@ -32,5 +34,29 @@ class RegisterServiceTest extends TestCase
         $this->assertEquals('test', $user->name);
         $this->assertEquals('test@example.com', $user->email);
         $this->assertTrue(Hash::check('password', $user->password));
+    }
+
+    /** @test */
+    public function it_can_register_user(): void
+    {
+        Event::fake();
+
+        $requestData = [
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'agree' => true,
+        ];
+        $request = new RegisterRequest($requestData);
+
+        $service = new RegisterService();
+        $registered = $service->register($request);
+
+        $this->assertTrue($registered);
+
+        Event::assertDispatched(Registered::class, static function ($event) {
+            return $event->user->email === 'test@example.com';
+        });
     }
 }
