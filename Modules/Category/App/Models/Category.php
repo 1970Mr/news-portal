f81812bc4d428a517133780a3d61c00b73f87856
1970Mr <mrepoch1970@gmail.com>
2024-03-29 20:44:03 +0430

    Create Tag module and add migrations and model

--- Modules/Category/App/Models/Category.php
+++ Modules/Category/App/Models/Category.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'name',
---
+++ Modules/Tag/App/Models/Tag.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Tag\App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Tag extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'status',
+    ];
+}
