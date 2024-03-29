b7608df37a3f5d04e3d110ed381144959a24a0d3
1970Mr <mrepoch1970@gmail.com>
2024-03-29 21:09:54 +0430

    Add views for Tag module

---
+++ Modules/Tag/resources/views/edit.blade.php
@@ -0,0 +1,90 @@
+@extends('panel::layouts.master', ['title' => 'ویرایش تگ'])
+
+@section('content')
+    <x-common-breadcrumbs>
+        <li><a href="{{ route('tag.index') }}">لیست تگ‌ها</a></li>
+        <li><a>ویرایش تگ</a></li>
+    </x-common-breadcrumbs>
+
+    <div class="row pe-0">
+        <div class="col-12 pe-0">
+            <div class="portlet box shadow min-height-500">
+                <div class="portlet-heading">
+                    <div class="portlet-title">
+                        <h3 class="title">
+                            <i class="icon-user-follow"></i>
+                            ویرایش تگ
+                        </h3>
+                    </div><!-- /.portlet-title -->
+                    <div class="buttons-box">
+                        <a class="btn btn-sm btn-default btn-round btn-fullscreen" rel="tooltip"
+                           aria-label="تمام صفحه" data-bs-original-title="تمام صفحه">
+                            <i class="icon-size-fullscreen d-flex justify-content-center align-items-center"></i>
+                            <div class="paper-ripple">
+                                <div class="paper-ripple__background"></div>
+                                <div class="paper-ripple__waves"></div>
+                            </div>
+                        </a>
+                    </div><!-- /.buttons-box -->
+                </div><!-- /.portlet-heading -->
+                <div class="portlet-body">
+                    <form id="tag-create-form" role="form" action="{{ route('tag.update', $tag->id) }}" method="post">
+                        @csrf
+                        <input type="hidden" name="_method" value="PUT">
+                        <x-common-error-messages />
+
+                        <fieldset class="row justify-content-center">
+                            <div class="form-group col-lg-6">
+                                <label for="name">نام <small>(ضروری)</small></label>
+                                <input id="name" class="form-control" name="name" type="text" required value="{{ old('name') ?? $tag->name }}">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="slug">slug <small>(ضروری)</small> </label>
+                                <input id="slug" class="form-control" name="slug" type="text" required value="{{ old('slug') ?? $tag->slug }}">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label for="description">توضیحات </label>
+                                <input id="description" class="form-control" name="description" type="text" value="{{ old('description') ?? $tag->description }}">
+                            </div>
+                            <div class="form-group text-center">
+                                <input id="status" class="form-control" name="status" type="checkbox" @if(old('status') || (!old('status') && $tag->status)) checked @endif>
+                                <label for="status">وضعیت</label>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-6 col-sm-offset-4 mx-auto">
+                                    <button class="btn btn-success btn-block">
+                                        <i class="icon-check"></i>
+                                        ویرایش تگ
+                                    </button>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div><!-- /.portlet-body -->
+            </div><!-- /.portlet -->
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <script>
+        $.validator.setDefaults({
+            highlight: function(element) {
+                $(element).closest('.form-group').addClass('has-error').removeClass("has-success");
+            },
+            unhighlight: function(element) {
+                $(element).closest('.form-group').removeClass('has-error').addClass("has-success");
+            },
+            errorElement: 'span',
+            errorClass: 'help-block',
+            errorPlacement: function(error, element) {
+                if(element.parent('.input-group').length) {
+                    error.insertAfter(element.parent());
+                } else {
+                    error.insertAfter(element);
+                }
+            }
+        });
+        $("#tag-create-form").validate();
+    </script>
+@endpush
--- Modules/Tag/resources/views/index.blade.php
+++ Modules/Tag/resources/views/index.blade.php
@@ -1,7 +1,104 @@
-@extends('tag::layouts.master')
+@extends('panel::layouts.master', ['title' => 'لیست دسته‌بندی‌ها'])
 
 @section('content')
-    <h1>Hello World</h1>
 
-    <p>Module: {!! config('tag.name') !!}</p>
+    <x-common-breadcrumbs>
+        <li><a>لیست دسته‌بندی‌ها</a></li>
+    </x-common-breadcrumbs>
+
+    <div class="row pe-0">
+        <div class="col-12 pe-0">
+            <div class="portlet box shadow min-height-500">
+                <div class="portlet-heading">
+                    <div class="portlet-title">
+                        <h3 class="title">
+                            <i class="icon-people"></i>
+                            لیست دسته‌بندی‌ها
+                        </h3>
+                    </div><!-- /.portlet-title -->
+                    <div class="buttons-box ltr">
+                        <a class="btn btn-sm btn-default btn-round btn-fullscreen" rel="tooltip"
+                           aria-label="تمام صفحه" data-bs-original-title="تمام صفحه">
+                            <i class="icon-size-fullscreen d-flex justify-content-center align-items-center"></i>
+                            <div class="paper-ripple">
+                                <div class="paper-ripple__background"></div>
+                                <div class="paper-ripple__waves"></div>
+                            </div>
+                        </a>
+                        @can(config('permissions_list.TAG_STORE'))
+                            <a class="btn btn-sm btn-default btn-round bg-green text-white" rel="tooltip"
+                               href="{{ route('tag.create') }}"
+                               aria-label="ایجاد دسته‌بندی‌ جدید" data-bs-original-title="ایجاد دسته‌بندی‌ جدید">
+                                <i class="icon-plus d-flex justify-content-center align-items-center"></i>
+                                <div class="paper-ripple">
+                                    <div class="paper-ripple__background"></div>
+                                    <div class="paper-ripple__waves"></div>
+                                </div>
+                            </a>
+                        @endcan
+                    </div><!-- /.buttons-box -->
+                </div><!-- /.portlet-heading -->
+                <div class="portlet-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped table-hover">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>نام</th>
+                                <th>slug</th>
+                                <th>توضیحات</th>
+                                <th>دسته‌بندی والد</th>
+                                <th>تاریخ ایجاد</th>
+                                <th>وضعیت</th>
+                                @canany([config('permissions_list.TAG_UPDATE'), config('permissions_list.TAG_DESTROY')])
+                                    <th>عملیات</th>
+                                @endcanany
+                            </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($tags as $tag)
+                                    <tr>
+                                        <td>{{ $tag->id }}</td>
+                                        <td>{{ $tag->name }}</td>
+                                        <td>{{ $tag->slug }}</td>
+                                        <td>{{ $tag->description }}</td>
+                                        <td class="ltr text-right">{{ jalalian()->forge($tag->created_at)->format(config('common.datetime_format')) }}</td>
+                                        <td class="{{ status_class($tag->status) }}">{{ status_message($tag->status) }}</td>
+                                        @canany([config('permissions_list.TAG_UPDATE'), config('permissions_list.TAG_DESTROY')])
+                                            <td class="d-flex gap-2">
+                                                @can(config('permissions_list.TAG_UPDATE'))
+                                                    <a class="btn btn-sm btn-info btn-icon round d-flex justify-content-center align-items-center"
+                                                       rel="tooltip" aria-label="ویرایش" data-bs-original-title="ویرایش" href="{{ route('tag.edit', $tag->id) }}">
+                                                        <i class="icon-pencil fa-flip-horizontal"></i>
+                                                    </a>
+                                                @endcan
+
+                                                @can(config('permissions_list.TAG_DESTROY'))
+                                                    <x-common-delete-button :route="route('tag.destroy', $tag->id)" />
+                                                @endcan
+
+                                            </td>
+                                        @endcanany
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <!-- Display pagination links -->
+                    {{ $tags->links() }}
+
+                </div><!-- /.portlet-body -->
+            </div><!-- /.portlet -->
+        </div>
+    </div>
+
 @endsection
+
+@push('styles')
+    <style>
+        .page-link{
+            text-align: center;
+        }
+    </style>
+@endpush
