ec954d0276dc63c2a04da33f44eb485049953caa
1970Mr <mrepoch1970@gmail.com>
2024-03-29 21:12:57 +0430

    Add permissions for Tag module

--- Modules/Role/config/permissions_list.php
+++ Modules/Role/config/permissions_list.php
@@ -2,17 +2,25 @@
 
 return [
     'SUPER_ADMIN' => 'admin::full_access',
+
     'USER_INDEX' => 'user::index',
     'USER_STORE' => 'user::store',
     'USER_UPDATE' => 'user::update',
     'USER_DESTROY' => 'user::destroy',
     'USER_ROLE_ASSIGNMENT' => 'user::role_assignment',
+
     'ROLE_INDEX' => 'role::index',
     'ROLE_STORE' => 'role::store',
     'ROLE_UPDATE' => 'role::update',
     'ROLE_DESTROY' => 'role::destroy',
+
     'CATEGORY_INDEX' => 'category::index',
     'CATEGORY_STORE' => 'category::store',
     'CATEGORY_UPDATE' => 'category::update',
     'CATEGORY_DESTROY' => 'category::destroy',
+
+    'TAG_INDEX' => 'tag::index',
+    'TAG_STORE' => 'tag::store',
+    'TAG_UPDATE' => 'tag::update',
+    'TAG_DESTROY' => 'tag::destroy',
 ];
