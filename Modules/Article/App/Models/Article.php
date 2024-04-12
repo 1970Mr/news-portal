675846d357881ff15e540d16fbfda1052c25ccc2
1970Mr <mrepoch1970@gmail.com>
2024-04-12 19:35:13 +0430

    Create article model and develop it in the Article module

--- Modules/Article/App/Models/Article.php
+++ Modules/Article/App/Models/Article.php
@@ -2,21 +2,62 @@
 
 namespace Modules\Article\App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Modules\Article\Database\factories\ArticleFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+use Modules\Category\App\Models\Category;
+use Modules\FileManager\App\Models\Image;
+use Modules\User\App\Models\User;
 
 class Article extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     */
-    protected $fillable = [];
-    
-    protected static function newFactory(): ArticleFactory
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'keywords',
+        'body',
+        'published_at',
+        'status',
+        'image_id',
+        'category_id',
+        'user_id',
+    ];
+
+    protected function slug(): Attribute
     {
-        //return ArticleFactory::new();
+        return Attribute::make(
+            set: static fn (string $value) => Str::slug($value),
+        );
+    }
+
+    public function scopeActive(Builder $query): void
+    {
+        $query->where('status', 1);
+    }
+
+    public function scopePublished(Builder $query): void
+    {
+        $query->where('published_at', '<=', now());
+    }
+
+    public function image(): BelongsTo
+    {
+        return $this->belongsTo(Image::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
