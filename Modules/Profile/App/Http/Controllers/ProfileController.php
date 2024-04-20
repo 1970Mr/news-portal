047c0db455e669e8c8420412a78ef320a90ece46
1970Mr <mrepoch1970@gmail.com>
2024-04-20 21:17:42 +0430

    Refactor profile controller in Profile module

--- Modules/Profile/App/Http/Controllers/ProfileController.php
+++ Modules/Profile/App/Http/Controllers/ProfileController.php
@@ -3,22 +3,28 @@
 namespace Modules\Profile\App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
-use Modules\Profile\App\Http\Requests\ChangePasswordRequest;
 use Modules\Profile\App\Http\Requests\ProfileRequest;
+use Modules\User\App\Models\User;
 
 class ProfileController extends Controller
 {
+    private Model $user;
+
+    public function __construct() {
+        $this->user = User::query()->find(auth()->id());
+    }
+
     public function edit(): View
     {
-        $user = auth()->user();
-        return view('profile::edit-profile', compact('user'));
+        return view('profile::edit-profile', ['user' => $this->user]);
     }
 
     public function update(ProfileRequest $request): RedirectResponse
     {
-        auth()->user()?->update($request->validated());
+        $this->user->update($request->validated());
         return to_route('profile.edit')->with('success', __('entity_edited', ['entity' => __('profile')]));
     }
 }
