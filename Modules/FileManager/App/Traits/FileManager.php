c59980e448394bc108ef44ba80051d75848c1a7d
1970Mr <mrepoch1970@gmail.com>
2024-04-21 23:28:52 +0430

    Fix error when creating long image name

--- Modules/FileManager/App/Traits/FileManager.php
+++ Modules/FileManager/App/Traits/FileManager.php
@@ -9,7 +9,13 @@ trait FileManager
 {
     public static function generateFilename(UploadedFile $file): string
     {
-        return $file->getClientOriginalName();
+        $filename = $file->getClientOriginalName();
+        if (strlen($filename) > 170) {
+            $extension = $file->getClientOriginalExtension();
+            $basename = substr($filename, 0, 150);
+            return $basename . '.' . $extension;
+        }
+        return $filename;
     }
 
     public static function generateFilePath($prefix = 'images'): string
