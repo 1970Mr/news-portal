b88522da277ee4493eb28135a77e04b9ada3160b
1970Mr <mrepoch1970@gmail.com>
2024-04-25 15:46:27 +0430

    Expand the dashboard view

--- Modules/Panel/App/Http/Controllers/PanelController.php
+++ Modules/Panel/App/Http/Controllers/PanelController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
 use Modules\Article\App\Models\Article;
 use Modules\Category\App\Models\Category;
+use Modules\Tag\App\Models\Tag;
 use Modules\User\App\Models\User;
 
 class PanelController extends Controller
@@ -18,12 +19,14 @@ class PanelController extends Controller
 
         $articles = Article::query()->latest()->limit(5)->get();
         $categories = Category::query()->latest()->limit(5)->get();
+        $tags = Tag::query()->latest()->limit(5)->get();
         return view('panel::index', compact([
             'users_count',
             'articles_count',
             'categories_count',
             'articles',
             'categories',
+            'tags',
         ]));
     }
 }
--- Modules/Panel/resources/views/index.blade.php
+++ Modules/Panel/resources/views/index.blade.php
@@ -64,7 +64,7 @@
 
     </div><!-- /.col-md-12 -->
 
-    <div class="row justify-content-center m-0 p-0">
+    <div class="row m-0 p-0">
         <div class="col-12">
             <div class="portlet box shadow min-height-500">
                 <div class="portlet-heading">
@@ -265,7 +265,50 @@
                     </div><!-- /.buttons-box -->
                 </div><!-- /.portlet-heading -->
                 <div class="portlet-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped table-hover">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>نام</th>
+                                <th>slug</th>
+                                <th>توضیحات</th>
+                                <th>تاریخ ایجاد</th>
+                                <th>وضعیت</th>
+                                @canany([config('permissions_list.TAG_UPDATE'), config('permissions_list.TAG_DESTROY')])
+                                    <th>عملیات</th>
+                                @endcanany
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($tags as $tag)
+                                <tr>
+                                    <td>{{ $tag->id }}</td>
+                                    <td>{{ $tag->name }}</td>
+                                    <td>{{ $tag->slug }}</td>
+                                    <td>{{ $tag->description }}</td>
+                                    <td class="ltr text-right">{{ jalalian()->forge($tag->created_at)->format(config('common.datetime_format')) }}</td>
+                                    <td class="{{ status_class($tag->status) }}">{{ status_message($tag->status) }}</td>
+                                    @canany([config('permissions_list.TAG_UPDATE'), config('permissions_list.TAG_DESTROY')])
+                                        <td class="d-flex gap-2">
+                                            @can(config('permissions_list.TAG_UPDATE', false))
+                                                <a class="btn btn-sm btn-info btn-icon round d-flex justify-content-center align-items-center"
+                                                   rel="tooltip" aria-label="ویرایش" data-bs-original-title="ویرایش" href="{{ route('tag.edit', $tag->id) }}">
+                                                    <i class="icon-pencil fa-flip-horizontal"></i>
+                                                </a>
+                                            @endcan
 
+                                            @can(config('permissions_list.TAG_DESTROY', false))
+                                                <x-common-delete-button :route="route('tag.destroy', $tag->id)" />
+                                            @endcan
+
+                                        </td>
+                                    @endcanany
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div><!-- /.portlet-body -->
             </div><!-- /.portlet -->
         </div>
