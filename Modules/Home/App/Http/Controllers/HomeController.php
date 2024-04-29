9a1f266fbc64aec7753d0a4366e1ecaf674b9d49
1970Mr <mrepoch1970@gmail.com>
2024-04-30 00:28:29 +0430

    Some rafactoring

--- Modules/Home/App/Http/Controllers/HomeController.php
+++ Modules/Home/App/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace Modules\Home\App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\View\View;
 use Modules\Article\App\Models\Article;
 
@@ -10,24 +11,22 @@ class HomeController extends Controller
 {
     public function __invoke(): View
     {
-        $ids_ignore = collect([]);
-        $default_value = Article::with(['hotness', 'image', 'category', 'tags'])->limit(5)->active()->published();
-
-        $trending_posts['editor_choices'] = Article::with(['hotness', 'image', 'category', 'tags'])->latest()->limit(3)->active()->published()->editorChoice()->get();
+        $trending_posts['editor_choices'] = $this->baseQuery()->editorChoice()->limit(3)->get();
         if ($trending_posts['editor_choices']->count() < 3) {
-            $trending_posts['editor_choices'] = $default_value->latest()->get()->shuffle()->take(3);
+            $trending_posts['editor_choices'] = $this->baseQuery()->get()->shuffle()->take(3);
         }
-        $ids_ignore = $ids_ignore->merge($trending_posts['editor_choices']->pluck('id'));
+        $ids_ignore = $trending_posts['editor_choices']->pluck('id');
         $trending_posts['first_editor_choice'] = $trending_posts['editor_choices']->pop();
 
-        $trending_posts['five_latest_articles'] = Article::with(['hotness', 'image', 'category', 'tags'])->whereNotIn('id', $ids_ignore)->latest()->limit(5)->active()->published()->get();
+        $trending_posts['five_latest_articles'] = $this->baseQuery()->whereNotIn('id', $ids_ignore)->limit(5)->get();
         if ($trending_posts['five_latest_articles']->count() < 1) {
-            $trending_posts['five_latest_articles'] = $default_value->get();
+            $trending_posts['five_latest_articles'] = $this->baseQuery()->limit(5)->get();
         }
-        $ids_ignore = $ids_ignore->merge($trending_posts['five_latest_articles']->pluck('id'));
+        return view('home::index', compact('trending_posts'));
+    }
 
-        return view('home::index', compact([
-            'trending_posts',
-        ]));
+    private function baseQuery(): Builder
+    {
+        return Article::with(['hotness', 'image', 'category', 'tags'])->latest()->active()->published();
     }
 }
