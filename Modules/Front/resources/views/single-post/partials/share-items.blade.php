0a1ef381997c826130f5013f2daa09c7daa4100e
1970Mr <mrepoch1970@gmail.com>
2024-05-04 00:25:34 +0430

    Set data to post content area in article index view

---
+++ Modules/Front/resources/views/single-post/partials/share-items.blade.php
@@ -0,0 +1,20 @@
+<div class="share-items clearfix">
+    <ul class="post-social-icons unstyled">
+        <li class="facebook">
+            <a href="#">
+                <i class="fa fa-facebook"></i> <span class="ts-social-title">فیسبوک</span></a>
+        </li>
+        <li class="twitter">
+            <a href="#">
+                <i class="fa fa-twitter"></i> <span class="ts-social-title">توییتر</span></a>
+        </li>
+        <li class="gplus">
+            <a href="#">
+                <i class="fa fa-google-plus"></i> <span class="ts-social-title">گوگل پلاس</span></a>
+        </li>
+        <li class="pinterest">
+            <a href="#">
+                <i class="fa fa-pinterest"></i> <span class="ts-social-title">Pinterest</span></a>
+        </li>
+    </ul>
+</div><!-- Share items end -->
