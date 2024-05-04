b6d14782af776d946a0a932467a707bab5bbd252
1970Mr <mrepoch1970@gmail.com>
2024-05-05 03:04:57 +0430

    Refactor the display boolean value

--- Modules/Comment/App/Http/Controllers/CommentController.php
+++ Modules/Comment/App/Http/Controllers/CommentController.php
@@ -5,9 +5,7 @@ namespace Modules\Comment\App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
-use Modules\Article\App\Models\Article;
 use Modules\Comment\App\Models\Comment;
 
 class CommentController extends Controller
