af33e763d296f9538760ace68255d129af40970f
1970Mr <mrepoch1970@gmail.com>
2024-05-04 20:55:54 +0430

    Set data for author box in article index view

--- Modules/Front/resources/views/single-post/index.blade.php
+++ Modules/Front/resources/views/single-post/index.blade.php
@@ -16,24 +16,7 @@
 
                     @include('front::single-post.partials.post-navigation')
 
-                    <div class="author-box">
-                        <div class="author-img pull-left">
-                            <img src="{{ asset('home/images/news/author.png') }}" alt="">
-                        </div>
-                        <div class="author-info">
-                            <h3>الهام طهماسبی</h3>
-                            <p class="author-url"><a href="#">لورم ایپسوم متن ساختگی با تولید</a></p>
-                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط</p>
-                            <div class="authors-social">
-                                <span>مرا دنبال کنید: </span>
-                                <a href="#"><i class="fa fa-behance"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-google-plus"></i></a>
-                                <a href="#"><i class="fa fa-pinterest-p"></i></a>
-                            </div>
-                        </div>
-                    </div> <!-- Author box end -->
+                    @include('front::single-post.partials.author-box')
 
                     <div class="related-posts block">
                         <h3 class="block-title"><span>مطالب مرتبط</span></h3>
