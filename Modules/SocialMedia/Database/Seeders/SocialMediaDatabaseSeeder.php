ce07a3fa542fdc6fea2613d3f6a57797b7ba6b00
1970Mr <mrepoch1970@gmail.com>
2024-05-10 12:51:17 +0430

    Add SocialMedia module

---
+++ Modules/SocialMedia/Database/Seeders/SocialMediaDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\SocialMedia\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class SocialMediaDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
