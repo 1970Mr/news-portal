a23236581dda2b702e2f5393b52e67f10c030776
1970Mr <mrepoch1970@gmail.com>
2024-05-15 16:35:59 +0430

    Add UserMessageRequest in ContactUs module

---
+++ Modules/ContactUs/App/Http/Requests/UserMessageRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\ContactUs\App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserMessageRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'email' => 'required|email|max:255',
+            'phone' => 'nullable|string|max:20',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string|min:3|max:1000',
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
