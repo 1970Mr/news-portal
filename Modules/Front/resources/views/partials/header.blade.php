79f59cfd3d9a26b971ff08a3954f138cf4b0f280
1970Mr <mrepoch1970@gmail.com>
2024-05-27 14:29:20 +0430

    Add header ads to header template

--- Modules/Front/resources/views/partials/header.blade.php
+++ Modules/Front/resources/views/partials/header.blade.php
@@ -2,14 +2,31 @@
 <header id="header" class="header text-center">
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <div class="logo">
-                    <a href="{{ route('home.index') }}">
-                        <img src="{{ $site_details->headerLogoLink() }}" alt="{{ config('app.name') }}">
-                    </a>
-                </div>
-            </div><!-- logo col end -->
+            @if(!$ads['header'])
+                <div class="col-12">
+                    <div class="logo">
+                        <a href="{{ route('home.index') }}">
+                            <img src="{{ $site_details->headerLogoLink() }}" alt="{{ config('app.name') }}">
+                        </a>
+                    </div>
+                </div><!-- logo col end -->
 
+            @else
+                <div class="col-xs-12 col-sm-3 col-md-3">
+                    <div class="logo">
+                        <a href="{{ route('home.index') }}">
+                            <img src="{{ $site_details->headerLogoLink() }}" alt="{{ config('app.name') }}">
+                        </a>
+                    </div>
+                </div><!-- logo col end -->
+
+                <div class="col-xs-12 col-sm-9 col-md-9 header-right">
+                    <div class="ad-banner pull-right">
+                        <a href="{{ $ads['header']->link }}"><img src="{{ asset('storage/' . $ads['header']->image->file_path) }}" class="img-responsive"
+                                         alt="{{ $ads['header']->image->alt_text }}"></a>
+                    </div>
+                </div><!-- header right end -->
+            @endif
         </div><!-- Row end -->
     </div><!-- Logo and banner area end -->
 </header><!--/ SiteDetail end -->
