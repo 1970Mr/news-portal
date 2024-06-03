ffa5bc2016e008d27c3054540d539dd030712577
1970Mr <mrepoch1970@gmail.com>
2024-06-03 21:32:58 +0430

    Adding all seeders in order in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,14 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Modules\Article\Database\Seeders\ArticleSeeder;
+use Modules\Category\Database\Seeders\CategorySeeder;
+use Modules\ContactUs\Database\Seeders\ContactInfoSeeder;
+use Modules\Setting\Database\Seeders\AboutUsSeeder;
+use Modules\Setting\Database\Seeders\SiteDetailSeeder;
+use Modules\Setting\Database\Seeders\SocialNetworkForSiteSeeder;
+use Modules\Tag\Database\Seeders\TagSeeder;
+use Modules\User\Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +20,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class,
+            TagSeeder::class,
+            ArticleSeeder::class,
+            ContactInfoSeeder::class,
+            SiteDetailSeeder::class,
+            AboutUsSeeder::class,
+            SocialNetworkForSiteSeeder::class,
+        ]);
     }
 }
