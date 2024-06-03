98e4e841e6e41bb01fedf591d8feb6876072117f
1970Mr <mrepoch1970@gmail.com>
2024-06-03 18:09:44 +0430

    Add tags to when seeding the articles

--- Modules/Article/Database/Seeders/ArticleSeeder.php
+++ Modules/Article/Database/Seeders/ArticleSeeder.php
@@ -31,15 +31,19 @@ class ArticleSeeder extends Seeder
             }
 
             $users = User::all();
-            Category::query()->whereDoesntHave('articles')->active()->limit(30)->each(function ($category) use ($users) {
+            $tags = Tag::all();
+            Category::query()->whereDoesntHave('articles')->active()->limit(30)->each(function ($category) use ($users, $tags) {
 //                $articlesCount = random_int(1, 10);
                 Article::factory(10)->create([
                     'category_id' => $category->id,
                     'user_id' => $users->random()->id,
-                ])->each(function ($article) {
+                ])->each(function ($article) use ($tags) {
                     if (random_int(0, 5) === 5) {
                         $article->hotness()->create(['is_hot' => true]);
                     }
+
+                    $randomTags = $tags->random(random_int(1, 8))->pluck('id')->toArray();
+                    $article->tags()->sync($randomTags);
                 });
             });
         });
