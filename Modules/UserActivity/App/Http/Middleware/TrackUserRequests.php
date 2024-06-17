04860af85c7fe956686c7008505c689adccaf3d7
1970Mr <mrepoch1970@gmail.com>
2024-06-18 00:21:35 +0430

    Refactor the UserActivity module

--- Modules/UserActivity/App/Http/Middleware/TrackUserRequests.php
+++ Modules/UserActivity/App/Http/Middleware/TrackUserRequests.php
@@ -3,15 +3,9 @@
 namespace Modules\UserActivity\App\Http\Middleware;
 
 use Closure;
-use DeviceDetector\ClientHints;
-use DeviceDetector\DeviceDetector;
-use DeviceDetector\Parser\Device\AbstractDeviceParser;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Modules\User\App\Models\User;
-use Modules\UserActivity\App\Models\RequestTrack;
-use Modules\UserActivity\App\Models\UserTrack;
+use Modules\UserActivity\App\Services\TrackUserRequestService;
 
 class TrackUserRequests
 {
@@ -25,73 +19,10 @@ class TrackUserRequests
             return $next($request);
         }
 
-        $track = $this->trackUserRequest($request, $user);
-        $this->createRequestTrack($request, $track);
+        $trackService = new TrackUserRequestService($request, $user);
+        $trackService->setUserTracking();
+        $trackService->createRequestTrack($request);
 
         return $next($request);
-    }
-
-    /**
-     * Track the user request.
-     */
-    private function trackUserRequest(Request $request, ?User $user): Model
-    {
-        $userId = $user ? $user->id : null;
-        $currentIp = $request->ip();
-        $deviceInfo = $this->getDeviceInfo($request->userAgent());
-        $deviceInfo['referer'] = $request->header('referer');
-
-        return $this->getOrCreateUserTrack($userId, $currentIp, $deviceInfo);
-    }
-
-    /**
-     * Get the device information from the user agent.
-     */
-    private function getDeviceInfo(string $userAgent): array
-    {
-        AbstractDeviceParser::setVersionTruncation(AbstractDeviceParser::VERSION_TRUNCATION_NONE);
-        $clientHints = ClientHints::factory($_SERVER);
-        $dd = new DeviceDetector($userAgent, $clientHints);
-        $dd->parse();
-
-        return [
-            'browser' => $dd->getClient('name') . ' ' . $dd->getClient('version'),
-            'device' => $dd->getDeviceName(),
-            'os' => $dd->getOs('name') . ' ' . $dd->getOs('version') . ' - ' . $dd->getOs('platform'),
-            'user_agent' => $dd->getUserAgent(),
-        ];
-    }
-
-    /**
-     * Get or create a user track record.
-     */
-    private function getOrCreateUserTrack(?int $userId, string $currentIp, array $deviceInfo): Model
-    {
-        return UserTrack::query()->firstOrCreate(
-            ['user_id' => $userId, 'ip' => $currentIp],
-            $deviceInfo
-        );
-    }
-
-    /**
-     * Create a new request track record.
-     */
-    private function createRequestTrack(Request $request, Model $track): void
-    {
-        RequestTrack::create([
-            'user_track_id' => $track->id,
-            'url' => $request->fullUrl(),
-            'referer' => $request->header('referer'),
-            'tag' => $this->getTag($request),
-        ]);
-    }
-
-    /**
-     * Get the tag based on the request URL.
-     */
-    private function getTag(Request $request): string
-    {
-        $panelPrefix = config('app.panel_prefix', 'panel');
-        return $request->is("{$panelPrefix}*") ? $panelPrefix : 'front';
     }
 }
---
+++ Modules/UserActivity/App/Services/TrackUserRequestService.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Modules\UserActivity\App\Services;
+
+use DeviceDetector\ClientHints;
+use DeviceDetector\DeviceDetector;
+use DeviceDetector\Parser\Device\AbstractDeviceParser;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Modules\User\App\Models\User;
+use Modules\UserActivity\App\Models\RequestTrack;
+use Modules\UserActivity\App\Models\UserTrack;
+
+class TrackUserRequestService
+{
+    public function __construct(
+        private readonly Request $request,
+        private ?User $user = null,
+        public ?Model $userTrack = null
+    ) {}
+
+    /**
+     * Track the user.
+     */
+    public function setUserTracking(): void
+    {
+        $userId = $this->user?->id;
+        $currentIp = $this->request->ip();
+        $deviceInfo = $this->getDeviceInfo($this->request->userAgent());
+        $deviceInfo['referer'] = $this->request->header('referer');
+
+        $this->userTrack = $this->getOrCreateUserTrack($userId, $currentIp, $deviceInfo);
+    }
+
+    /**
+     * Get The User Track.
+     */
+    public function getUserTracking(): Model
+    {
+        if ($this->userTrack) {
+            $this->setUserTracking();
+        }
+        return $this->userTrack;
+    }
+
+    /**
+     * Get the device information from the user agent.
+     */
+    private function getDeviceInfo(string $userAgent): array
+    {
+        AbstractDeviceParser::setVersionTruncation(AbstractDeviceParser::VERSION_TRUNCATION_NONE);
+        $clientHints = ClientHints::factory($_SERVER);
+        $dd = new DeviceDetector($userAgent, $clientHints);
+        $dd->parse();
+
+        return [
+            'browser' => $dd->getClient('name') . ' ' . $dd->getClient('version'),
+            'device' => $dd->getDeviceName(),
+            'os' => $dd->getOs('name') . ' ' . $dd->getOs('version') . ' - ' . $dd->getOs('platform'),
+            'user_agent' => $dd->getUserAgent(),
+        ];
+    }
+
+    /**
+     * Get or create a user track record.
+     */
+    private function getOrCreateUserTrack(?int $userId, string $currentIp, array $deviceInfo): Model
+    {
+        return UserTrack::query()->firstOrCreate(
+            ['user_id' => $userId, 'ip' => $currentIp],
+            $deviceInfo
+        );
+    }
+
+    /**
+     * Create a new request track record.
+     */
+    public function createRequestTrack(Request $request): void
+    {
+        RequestTrack::create([
+            'user_track_id' => $this->getUserTracking()->id,
+            'url' => $request->fullUrl(),
+            'referer' => $request->header('referer'),
+            'tag' => $this->getTag($request),
+        ]);
+    }
+
+    /**
+     * Get the tag based on the request URL.
+     */
+    private function getTag(Request $request): string
+    {
+        $panelPrefix = config('app.panel_prefix', 'panel');
+        return $request->is("{$panelPrefix}*") ? $panelPrefix : 'front';
+    }
+}
