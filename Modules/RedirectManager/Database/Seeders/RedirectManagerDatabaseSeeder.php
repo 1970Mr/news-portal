0fd13c7754a70ec3e2d5ebfd18d835ae6f8237cb
1970Mr <mrepoch1970@gmail.com>
2024-06-19 10:55:35 +0430

    Add RedirectManager module

---
+++ Modules/RedirectManager/Database/Seeders/RedirectManagerDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\RedirectManager\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RedirectManagerDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/RedirectManager/resources/views/show.blade.php
@@ -0,0 +1,7 @@
+@extends('redirectmanager::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('redirectmanager.name') !!}</p>
+@endsection
